114e62a87d363fe065927fced8e22c2a2b7da815
mizisu <alwltn960406@gmail.com>
2020-07-12 20:16:56 +0900

    Modified model

--- users/models.py
+++ users/models.py
@@ -8,25 +8,12 @@ class Users(models.Model):
     """
 
     id = fields.IntField(pk=True)
-    #: This is a username
     username = fields.CharField(max_length=20, unique=True)
-    name = fields.CharField(max_length=50, null=True)
-    family_name = fields.CharField(max_length=50, null=True)
-    category = fields.CharField(max_length=30, default="misc")
     password_hash = fields.CharField(max_length=128, null=True)
-    created_at = fields.DatetimeField(auto_now_add=True)
-    modified_at = fields.DatetimeField(auto_now=True)
-
-    def full_name(self) -> str:
-        """
-        Returns the best name
-        """
-        if self.name or self.family_name:
-            return f"{self.name or ''} {self.family_name or ''}".strip()
-        return self.username
+    timestamp_created = fields.DatetimeField(auto_now_add=True)
+    timestamp_modified = fields.DatetimeField(auto_now=True)
 
     class PydanticMeta:
-        computed = ["full_name"]
         exclude = ["password_hash"]
 
 
