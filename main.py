bbb9d29292567a70a054f7d410809db99677355a
mizisu <alwltn960406@gmail.com>
2020-07-14 22:17:57 +0900

    Run uvicorn on main for debugging

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import uvicorn
 from fastapi import FastAPI
 
 from core import Loader
@@ -13,3 +14,6 @@ loader.load_apps(
     "users",
     "todos",
 )
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
