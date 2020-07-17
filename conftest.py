6c09af229dcb5b895e449aa0ae6f40ae25f805bc
mizisu <alwltn960406@gmail.com>
2020-07-17 19:44:52 +0900

    Setup pytest

--- conftest.py
+++ conftest.py
@@ -1,13 +1,14 @@
 from typing import Generator
 
+import pytest
 from fastapi.testclient import TestClient
 from tortoise.contrib.test import finalizer, initializer
 from main import app
 
 
 @pytest.fixture(scope="module")
-def client() -> Generator:
-    initializer(["models"])
+def client():
+    initializer(["users.models"])
     with TestClient(app) as c:
         yield c
     finalizer()
