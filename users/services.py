a7f9d6a9255826fd8b428f1a15a82b4ed9c17417
mizisu <alwltn960406@gmail.com>
2020-07-26 23:47:04 +0900

    WIP impl login

--- users/services.py
+++ users/services.py
@@ -14,3 +14,11 @@ async def create_user(username: str, password: str):
 
 def get_hash_password(password: str):
     return bcrypt.hashpw(password.encode('UTF-8'), bcrypt.gensalt())
+
+
+async def login(username: str, password: str):
+    user = User.get_or_none(username=username)
+    if user is None:
+        raise 404
+
+    return bcrypt.checkpw(password, user.password)
