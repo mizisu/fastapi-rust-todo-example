1747733f6c1021779a5dfab3ebc68386449b3855
mizisu <alwltn960406@gmail.com>
2020-08-06 21:27:06 +0900

    WIP impl todo apis

--- todos/models.py
+++ todos/models.py
@@ -1,5 +1,4 @@
 from tortoise import fields, models
-from tortoise.contrib.pydantic import pydantic_model_creator
 
 
 class Todo(models.Model):
@@ -7,10 +6,3 @@ class Todo(models.Model):
     content = fields.TextField()
     timestamp_created = fields.DatetimeField(auto_now_add=True)
     timestamp_updated = fields.DatetimeField(auto_now=True)
-
-
-
-todo = pydantic_model_creator(Todo)
-todo_create = pydantic_model_creator(
-    Todo,
-)
