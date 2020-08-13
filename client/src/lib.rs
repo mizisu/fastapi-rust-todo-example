47047dca579acf16303044afcb2ff8943efa1f97
mizisu <alwltn960406@gmail.com>
2020-08-14 01:18:07 +0900

    Added index.html

--- client/src/lib.rs
+++ client/src/lib.rs
@@ -1,7 +1,30 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+use wasm_bindgen::prelude::*;
+use yew::prelude::*;
+
+struct Hello {}
+
+impl Component for Hello {
+    type Message = ();
+    type Properties = ();
+
+    fn create(props: Self::Properties, link: ComponentLink<Self>) -> Self {
+        Self {}
     }
+
+    fn update(&mut self, msg: Self::Message) -> ShouldRender {
+        true
+    }
+
+    fn change(&mut self, _props: Self::Properties) -> ShouldRender {
+        true
+    }
+
+    fn view(&self) -> Html {
+        html! { <span>{"Hello World!"}</span> }
+    }
+}
+
+#[wasm_bindgen(start)]
+pub fn run_app() {
+    App::<Hello>::new().mount_to_body();
 }
