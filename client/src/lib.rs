32d3cb06e79b1023a6e6c25456c6a23a50d6c810
mizisu <alwltn960406@gmail.com>
2020-08-16 21:57:33 +0900

    WIP added login page

--- client/src/lib.rs
+++ client/src/lib.rs
@@ -1,10 +1,10 @@
 mod pages;
 
+use pages::Login;
 use wasm_bindgen::prelude::*;
 use yew::prelude::*;
 
-
 #[wasm_bindgen(start)]
 pub fn run_app() {
-    App::<>::new().mount_to_body();
+    App::<Login>::new().mount_to_body();
 }
---
+++ client/src/pages/todo_list.rs
@@ -0,0 +1,51 @@
+use yew::prelude::*;
+
+struct Todo {
+    id: i32,
+    content: String,
+    completed: bool,
+}
+
+struct State {
+    todos: Vec<Todo>,
+}
+
+pub struct TodoList {
+    state: State,
+}
+
+impl Component for TodoList {
+    type Message = ();
+    type Properties = ();
+
+    fn create(_: Self::Properties, _: ComponentLink<Self>) -> Self {
+        let todos = vec![
+            Todo {
+                id: 1,
+                content: "content1".to_string(),
+                completed: false,
+            },
+            Todo {
+                id: 2,
+                content: "content2".to_string(),
+                completed: false,
+            },
+        ];
+
+        Self {
+            state: State { todos },
+        }
+    }
+
+    fn update(&mut self, _: Self::Message) -> ShouldRender {
+        true
+    }
+
+    fn change(&mut self, _: Self::Properties) -> ShouldRender {
+        true
+    }
+
+    fn view(&self) -> Html {
+        html! { <span>{""}</span> }
+    }
+}
