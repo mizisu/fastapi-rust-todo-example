d8d44290be02f24eaac2cda5e419fb51366d5eed
mizisu <alwltn960406@gmail.com>
2020-09-21 22:00:15 +0900

    Connect form submit event

--- client/src/components/text_input.rs
+++ client/src/components/text_input.rs
@@ -1,20 +1,33 @@
 use yew::prelude::*;
+use yew::events::Event;
+use yew::services::ConsoleService;
+
+pub enum Msg {
+  Submit,
+}
 
 pub struct TextInput {
   state: State,
+  link: ComponentLink<Self>,
 }
 
 struct State {}
 
 impl Component for TextInput {
-  type Message = ();
+  type Message = Msg;
   type Properties = ();
 
-  fn create(_: Self::Properties, _: ComponentLink<Self>) -> Self {
-    Self { state: State {} }
+  fn create(_: Self::Properties, link: ComponentLink<Self>) -> Self {
+    Self { state: State {}, link }
   }
 
-  fn update(&mut self, _: Self::Message) -> ShouldRender {
+  fn update(&mut self, msg: Self::Message) -> ShouldRender {
+    match msg {
+      Msg::Submit => {
+        println!("Submit!")
+      }
+    }
+
     true
   }
 
@@ -23,9 +36,15 @@ impl Component for TextInput {
   }
 
   fn view(&self) -> Html {
+    let submit = self.link.callback(|e: FocusEvent|{
+      e.prevent_default();
+      ConsoleService::info("Update:");
+      Msg::Submit
+    });
+
     html! {
     <div class="row">
-      <form class="col s12">
+      <form class="col s12" onsubmit=submit>
         <div class="row">
           <div class="input-field col s12">
           <input id="todo" type="text" class="validate" />
