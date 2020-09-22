1b1c9fc33d53436e053a58599ab89d4194d0d783
mizisu <alwltn960406@gmail.com>
2020-09-22 21:58:20 +0900

    Connect input event

--- client/src/components/text_input.rs
+++ client/src/components/text_input.rs
@@ -1,9 +1,10 @@
 use yew::prelude::*;
-use yew::events::Event;
+use yew::events::{Event, InputData};
 use yew::services::ConsoleService;
 
 pub enum Msg {
   Submit,
+  ValueUpdate(String),
 }
 
 pub struct TextInput {
@@ -11,20 +12,27 @@ pub struct TextInput {
   link: ComponentLink<Self>,
 }
 
-struct State {}
+struct State {
+  value: String,
+}
 
 impl Component for TextInput {
   type Message = Msg;
   type Properties = ();
 
   fn create(_: Self::Properties, link: ComponentLink<Self>) -> Self {
-    Self { state: State {}, link }
+    Self { state: State {
+      value: "".to_string(),
+    }, link }
   }
 
   fn update(&mut self, msg: Self::Message) -> ShouldRender {
     match msg {
       Msg::Submit => {
         println!("Submit!")
+      },
+      Msg::ValueUpdate(value) => {
+        self.state.value = value;
       }
     }
 
@@ -36,18 +44,20 @@ impl Component for TextInput {
   }
 
   fn view(&self) -> Html {
-    let submit = self.link.callback(|e: FocusEvent|{
+    let onsubmit = self.link.callback(|e: FocusEvent|{
       e.prevent_default();
-      ConsoleService::info("Update:");
       Msg::Submit
     });
 
     html! {
     <div class="row">
-      <form class="col s12" onsubmit=submit>
+      <form class="col s12" onsubmit=onsubmit>
         <div class="row">
           <div class="input-field col s12">
-          <input id="todo" type="text" class="validate" />
+          <input id="todo" type="text" class="validate" 
+                 value=&self.state.value
+                 oninput=self.link.callback(|e : InputData| Msg::ValueUpdate(e.value))
+           />
           <label for="todo">{"New Task"}</label>
           </div>
         </div>
